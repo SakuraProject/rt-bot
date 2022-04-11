dd67f9037ff96f688c28a94dce5399030e4b2720
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-04-11 22:38:31 +0900

    Add files via upload

--- cogs/transit.py
+++ cogs/transit.py
@@ -10,11 +10,11 @@ class transit(commands.Cog):
         self.bot = bot
 
     @commands.command(name="transit")
-    async def transit(self,ctx,depature,to):
+    async def transit(self, ctx, depature, to):
         async with aiohttp.ClientSession() as session:
             async with session.get("https://ysmsrv.wjg.jp/transit/index_raw.php?from="+urllib.parse.quote_plus(depature, encoding='utf-8')+"&to="+urllib.parse.quote_plus(to, encoding='utf-8')) as resp:
-                sid=await resp.text()
-                ssplit=sid.splitlines()
+                sid = await resp.text()
+                ssplit = sid.splitlines()
                 ssplit.sort(key=len)
                 embed = discord.Embed(title=depature+"駅から"+to+"駅までの行き方", description=ssplit[0], color=0x0066ff)
                 embed.set_footer(text="乗り換え案内")
