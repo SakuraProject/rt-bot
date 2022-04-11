455ed35df960a3b6104f0e556000a58c851b7424
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-04-11 22:19:54 +0900

    urllib.request to aiohttp

--- cogs/transit.py
+++ cogs/transit.py
@@ -1,7 +1,7 @@
 # Free RT - transit
 
 from discord.ext import commands
-import urllib.request
+import aiohttp
 import urllib.parse
 import discord
 
@@ -11,13 +11,14 @@ class transit(commands.Cog):
 
     @commands.command(name="transit")
     async def transit(self,ctx,depature,to):
-        with urllib.request.urlopen("https://ysmsrv.wjg.jp/transit/index_raw.php?from="+urllib.parse.quote_plus(depature, encoding='utf-8')+"&to="+urllib.parse.quote_plus(to, encoding='utf-8')) as u:
-            sid=u.read().decode()
-            ssplit=sid.splitlines()
-            ssplit.sort(key=len)
-            embed = discord.Embed(title=depature+"駅から"+to+"駅までの行き方", description=ssplit[0], color=0x0066ff)
-            embed.set_footer(text="乗り換え案内")
-            await ctx.send(embed=embed)
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://ysmsrv.wjg.jp/transit/index_raw.php?from="+urllib.parse.quote_plus(depature, encoding='utf-8')+"&to="+urllib.parse.quote_plus(to, encoding='utf-8')) as resp:
+                sid=await resp.text()
+                ssplit=sid.splitlines()
+                ssplit.sort(key=len)
+                embed = discord.Embed(title=depature+"駅から"+to+"駅までの行き方", description=ssplit[0], color=0x0066ff)
+                embed.set_footer(text="乗り換え案内")
+                await ctx.send(embed=embed)
 
 
 
