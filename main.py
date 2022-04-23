398f204b6b786b1dcc6e2bc5aabb7f7cd3837fdf
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-04-23 17:45:11 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,109 @@
+"""Free RT Backend (C) 2022 Free RT
+LICENSE : ./LICENSE
+README  : ./readme.md
+"""
+
+print("Free RT Discord Bot (C) 2022 Free RT\nNow loading...")
+
+from os import listdir
+from sys import argv
+
+from logging import handlers
+import logging
+
+import discord
+
+from aiohttp import ClientSession
+from ujson import load, dumps
+
+from util import RT, mysql, setup, websocket
+from data import data, Colors
+
+with open("auth.json", "r") as f:
+    secret = load(f)
+
+# Botの準備を行う。
+intents = discord.Intents.default()  # intents指定
+intents.typing = False
+intents.members = True
+bot = RT(
+    data["prefixes"][argv[-1]],
+    help_command=None,
+    intents=intents,
+    allowed_mentions=discord.AllowedMentions(
+        everyone=False,
+        users=False,
+        replied_user=False
+    ),
+    activity=discord.Game("起動準備"),
+    status=discord.Status.dnd)  # RTオブジェクトはcommands.Botを継承している
+bot.test = argv[-1] != "production"  # argvの最後がproductionかどうか
+if not bot.test:
+    websocket.WEBSOCKET_URI_BASE = "ws://146.59.153.178"
+bot.data = data  # 全データアクセス用、非推奨
+bot.owner_ids = data["admins"]
+bot.secret = secret  # auth.jsonの内容を入れている
+bot.mysql = bot.data["mysql"] = mysql.MySQLManager(
+    loop=bot.loop,
+    **secret["mysql"],
+    pool=True,
+    minsize=1,
+    maxsize=500 if bot.test else 1000000,
+    autocommit=True
+)  # maxsizeはテスト用では500、本番環境では100万になっている
+bot.pool = bot.mysql.pool  # bot.mysql.pool のエイリアス
+bot.colors = data["colors"]  # 下のColorsを辞書に変換したもの
+bot.Colors = Colors  # botで使う基本色が入っているclass
+
+# 起動中だと教えられるようにするためのコグを読み込む
+bot.load_extension("cogs._first")
+# スラッシュマネージャーを設定する
+bot.load_extension("util.slash")
+# onami(jishakuのnextcord版)を読み込む
+bot.load_extension("onami")
+
+
+@bot.listen()
+async def on_ready():
+    bot.print("Connected to discord")
+    # 起動中いつでも使えるaiohttp.ClientSessionを作成
+    bot.session = ClientSession(loop=bot.loop, json_serialize=dumps)
+    bot.unload_extension("cogs._first")
+
+    # 拡張を読み込む
+    setup(bot)  # util.setup
+    bot.load_extension("cogs._oldrole")  # oldroleだけ特別に読み込んでいる
+    for name in listdir("cogs"):
+        if not name.startswith(("_", ".")):
+            try:
+                bot.load_extension(
+                    f"cogs.{name[:-3] if name.endswith('.py') else name}")
+            except discord.ext.commands.NoEntryPointError as e:
+                if "setup" not in str(e):
+                    raise e
+            else:
+                bot.print("[Extension]", "Loaded", name)  # ロードログの出力
+    bot.print("Completed to boot Free RT")
+
+    bot.dispatch("full_ready")  # full_readyイベントを発火する
+
+
+# loggingの準備
+#logger = logging.getLogger('discord')
+#handler = handlers.RotatingFileHandler(
+#    filename='log/discord.log',
+#    encoding='utf-8',
+#    mode='w',
+#    maxBytes=10000000,
+#    backupCount=5
+#)  # 出力先ファイルの設定
+#handler.setLevel(logging.DEBUG)  # 出力レベルの設定
+#handler.setFormatter(
+#    logging.Formatter(
+#        "[%(asctime)s][%(levelname)s][%(name)s] %(message)s"
+#    )
+#)  # 出力形式の設定
+#logger.addHandler(handler)
+
+# 実行
+bot.run(secret["token"][argv[-1]])
---
+++ readme.md
@@ -0,0 +1,39 @@
+<!--[![Discord Bots](https://top.gg/api/widget/status/716496407212589087.svg)](https://top.gg/bot/716496407212589087) [![Discord Bots](https://top.gg/api/widget/servers/716496407212589087.svg)](https://top.gg/bot/716496407212589087) ![Discord](https://img.shields.io/discord/718641964672876614?label=support&logo=discord)-->
+<!-- To do: この部分をFreeRTのbotや公式サーバーのものにする。-->
+(日本語版のreadmeは[こちら](https://github.com/Free-RT/rt-bot/blob/main/readme.ja.md))
+# Free RT Bot
+This is an Discord's bot, Free RT.  
+There is 'RT bot' in discord and we can use it for paying to tasuren, so please use RT if you have enough money.  
+Free RT is a feature-rich bot with features that most bots have.  
+It also has features that other bots don't have.  
+Connect to Free RT, Discord's Bot account, to start Free RT's service.  
+It also communicates with `rt-backend` via WebSocket for web authentication and so on.(But now do not and we are making. It will be coming soon.)  
+If you don't know about RT, have a look at [here](https://rt-bot.com/).  
+If you don't know about Free RT, have a look at [here](https://free-rt.com/).  
+
+## LICENSE
+`BSD 4-Clause License` (The `LICENSE` file has more details.)
+
+## CONTRIBUTION
+See [contributing](https://github.com/Free-RT/rt-bot/blob/main/contributing/).
+
+## Installation.
+## Dependencies.
+Required.
+
+* Python 3.10
+* MySQL or MariaDB.
+* Everything in `requirements.txt`.
+* Run `rt-backend` if you want to use functions that require a backend such as authentication.
+### Startup procedure: 1.
+1. install required items with `pip install -r requirements.txt` 2.
+2. write necessary TOKEN etc. to `auth.json` referring to `auth.template.json`. 3.
+Put the `rt-module` repository in `util` and name the folder `rt_module`. 4.
+4. run the program in the `rt-backend` repository.
+   (This is optional and you need to do it if you want to run something that requires a backend such as authentication. 5.)
+Run the tests with `python3 main.py test`.
+   (In this case, TOKEN will be the one in the `test` key of `auth.json`.)
+
+(In this case, TOKEN is the one in the key of `test` in `auth.json`.) * Read `readme.md` in `cogs/tts` if you want to run the readout.
+### Run production.
+The startup command is `sudo -E python3 main.py production` and you need `auth.json` TOKEN for `production`.
