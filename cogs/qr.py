87b4119db2090b4fade7033f2ec094ac756fe595
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-04-29 23:38:51 +0900

    fix: Lint

--- cogs/qr.py
+++ cogs/qr.py
@@ -14,15 +14,15 @@ class qr(commands.Cog):
 
     @commands.group(
         extras={
-            "headding":{"ja":"qr関連のコマンドです", "en":"qr command"},
-            "parent":"Entertainment"
+            "headding": {"ja": "qr関連のコマンドです", "en": "qr command"},
+            "parent": "Entertainment"
         }
     )
     async def qr(self, ctx): 
         """!lang ja
         --------
         qrコード関連のコマンドです。sub_commands: make, read
-        
+
         !lang en
         --------
         qr category command。sub_commands: make, read"""
@@ -31,37 +31,37 @@ class qr(commands.Cog):
 
     @qr.command(
         extras={
-            "headding":{"ja":"文字からqrコードを作成します", "en":"making qr code on string"},
-            "parent":"Entertainment"
+            "headding": {"ja": "文字からqrコードを作成します", "en": "making qr code on string"},
+            "parent": "Entertainment"
         }
     )
     async def make(self, ctx, text): 
-        a = pyqrcode.create(content=text,error='H') 
-        a.png(file=str(ctx.author.id)+'.png',scale=6)
-        await ctx.send(file=discord.File(str(ctx.author.id)+'.png'))
-        os.remove(str(ctx.author.id)+'.png')
+        a = pyqrcode.create(content=text, error='H') 
+        a.png(file=str(ctx.author.id) + '.png', scale=6)
+        await ctx.send(file=discord.File(str(ctx.author.id) + '.png'))
+        os.remove(str(ctx.author.id) + '.png')
 
     @qr.command(
         extras={
-            "headding":{"ja":"qrコードを読み取ります", "en":"read qr code"},
-            "parent":"Entertainment"
+            "headding": {"ja":"qrコードを読み取ります", "en": "read qr code"},
+            "parent": "Entertainment"
         }
     )
     async def read(self, ctx, url=None): 
-        if url==None: 
+        if url is None: 
             url = ctx.message.attachments[0].url
         async with self.bot.session.get(url) as resp:
-            with open(str(ctx.author.id)+'r.png', 'wb') as fp:
+            with open(str(ctx.author.id) + 'r.png', 'wb') as fp:
                 while True: 
-                    r = await resp.content.read(10) #4
+                    r = await resp.content.read(10)  # 4
                     if not r: 
                         break
                     fp.write(r)
-        image = cv2.imread(str(ctx.author.id)+'r.png')
+        image = cv2.imread(str(ctx.author.id) + 'r.png')
         qrDetector = cv2.QRCodeDetector()
         data,bbox,rectifiedImage = qrDetector.detectAndDecode(image)
         await ctx.send(data)
-        os.remove(str(ctx.author.id)+'r.png')
+        os.remove(str(ctx.author.id) + 'r.png')
 
 def setup(bot): 
     bot.add_cog(qr(bot))
