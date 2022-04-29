5394b3f3536f563031f31120b875e65b7c3ac4fc
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-04-30 08:57:19 +0900

    Update schedule.py

--- cogs/schedule.py
+++ cogs/schedule.py
@@ -8,7 +8,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from aiomysql import Pool
 
-TABLES = ("schedule","schedule_test")
+TABLES = ("schedule", "schedule_test")
 
 
 class DataManager:
